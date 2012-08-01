706a34dfe22f96957705d9c6f39a7e7d6c034b0b
alexander.sergeychik@gmail.com <omez@dev.(none)>
2012-08-01 20:09:51 +0300

    Full build of v1.1.5

---
+++ bin/doctrine-boot.php
@@ -0,0 +1,50 @@
+<?php
+namespace Doctrine2zf;
+
+use Zend\Mvc\Application;
+use Zend\ServiceManager\ServiceManager;
+
+
+if (!function_exists('lookForBasePath')) {
+	function lookForBasePath($path, $from = __DIR__) {
+		$previousFrom = $from;
+		while (true) {
+			$fullpath = $from . DIRECTORY_SEPARATOR . $path;
+			if (file_exists($fullpath)) {
+				return $from;
+			} else {
+				$previousFrom = $from;
+				$from = $from.'/..';
+				if (realpath($previousFrom) === realpath($from)) {
+					return false;
+				}
+			}
+		}
+	}
+}
+
+
+$basePath = lookForBasePath('boot/boot.php');
+if ($basePath !== false) {
+	chdir($basePath);
+	$serviceManager = require 'boot/boot.php';
+	return $serviceManager;
+}
+
+
+$basePath = lookfor('config/application.config.php');
+if ($basePath !== false) {
+	chdir($basePath);
+	
+	// looking for application vendor autoloader 
+	if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !(@include_once __DIR__ . '/../../../autoload.php')) {
+		throw new \RuntimeException('Error: vendor/autoload.php could not be found. Did you run php composer.phar install?');
+	}
+	
+	$application = Application::init(include 'config/application.config.php');
+	$serviceManager = $application->getServiceManager();
+	return $serviceManager;
+}
+
+// if no return we have failed 
+throw new \RuntimeException("Unable to bootstrap doctrine application");
--- bin/doctrine.php
+++ bin/doctrine.php
@@ -1,14 +1,14 @@
 <?php
 use Zend\ServiceManager\ServiceManager;
-/**
- * Before running of this script variable $serviceManager should be set
- * 
- * @var ServiceManager $serviceManager
- */
-
-$serviceManager =isset($serviceManager)?$serviceManager:NULL;
 
 ini_set('display_errors', true);
+
+/**
+ * Before running of this script variable $serviceManager should be set
+ *
+ * @var $serviceManager $serviceManager
+ */
+$serviceManager = include __DIR__.'/doctrine-boot.php';
 
 if (!isset($serviceManager)) {
 	throw new RuntimeException('Service manager not set');
@@ -16,5 +16,4 @@ if (!isset($serviceManager)) {
 	throw new RuntimeException('Service manager registered but not instance of ServiceManager');
 }
 
-$serviceManager->get('Application')->bootstrap();
 $serviceManager->get('doctrine.cli')->run();
